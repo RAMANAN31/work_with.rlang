f810e34a2042089d1d56fa17a5ce277302061b04
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-09-15 16:23:00 +0530

    Create regression.r

---
+++ regression.r
@@ -0,0 +1,22 @@
+#Regression Analysis
+X=c(23,27,28,28,29,30,31,33,35,36)
+Y=c(18,20,22,27,21,29,27,29,28,29)
+cor(X,Y)
+
+#Regression equation Y on X
+X=c(23,27,28,28,29,30,31,33,35,36)
+Y=c(18,20,22,27,21,29,27,29,28,29)
+Regr=lm(Y~X) # lm means linear model
+Regr
+
+plot(X,Y,main="X data vs. Y data",xlab="x data",ylab="Y data",col="red")
+abline(lm(Y~X))
+
+#Regression equation X on Y
+Regr=lm(X~Y) # lm means linear model
+Regr
+
+plot(Y,X,main="X data vs. Y data",xlab="Y data",ylab="X data",col="red")
+abline(lm(X~Y))
+
+"""
